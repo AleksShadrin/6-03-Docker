f3282a7716e9a8e01ec3cb9745e9109112d5f0ea
AleksShadrin <122561392+AleksShadrin@users.noreply.github.com>
2023-02-16 00:47:39 +0300

    Update 4.sh

--- 5-03-Regexp/4.sh
+++ 5-03-Regexp/4.sh
@@ -5,9 +5,13 @@ logfile=$1
 #timestamp loadavg1 loadavg5 loadavg15 memfree memtotal diskfree disktotal
 while true;
 do    
-    echo date >> $logfile
+    loadavg1 = cat /proc/loadavg | awk '{print $1}'
+    loadavg5
+    loadavg15
     cat /proc/loadavg | echo >> logfile 
     free | echo >> logfile
+    
+    echo date loadavg1 loadavg5 loadavg15 memfree memtotal diskfree disktotal >> $logfile
     sleep 5
 done
 #loadavg[1,5,15] средний показатель загрузки ЦПУ за последние 1 5 и 15 минут. Примечание: хранится в /proc/loadavg.
