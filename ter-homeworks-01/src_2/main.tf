f19f5c596912a9f95866287be435a254f6be6088
AleksShadrin <122561392+AleksShadrin@users.noreply.github.com>
2024-01-25 01:33:09 +0300

    Update main.tf

--- ter-homeworks-01/src_2/main.tf
+++ ter-homeworks-01/src_2/main.tf
@@ -6,3 +6,52 @@ terraform {
   }
   required_version = ">= 0.13"
 }
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "vm-1"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 4
+    core_fraction = 5
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd89iq8mqvli97d9poej"
+      size = 10
+    }
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = {
+    user-data = "${file("/mnt/shared/terraform/metadata.txt")}"
+  }
+
+    network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
